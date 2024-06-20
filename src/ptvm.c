4f5ff838449a05ed7911990a2ac73fe241f886af
HCO3 <146848791+hydrogenbicarbonate@users.noreply.github.com>
2024-06-20 10:44:44 +0000

    Implemented a very basic virtual machine with simple Instructions like PUSH and POP

--- src/ptvm.c
+++ src/ptvm.c
@@ -1,11 +1,11 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
 typedef enum {
   INST_PUSH,
-  INST_POP, 
+  INST_POP,
   INST_ADD,
   INST_SUB,
   INST_MUL,
@@ -18,88 +18,84 @@ typedef struct {
   int value;
 } Inst;
 
+#define DEF_INST_PUSH(x) { .type = INST_PUSH, .value = x }
+#define DEF_INST_POP { .type = INST_POP }
+#define DEF_INST_ADD() { .type = INST_ADD }
+#define DEF_INST_SUB() { .type = INST_SUB }
+#define DEF_INST_DIV() { .type = INST_DIV }
+#define DEF_INST_MUL() { .type = INST_MUL }
+ 
 Inst program[] = {
-  {.type = INST_PUSH, .value = 15},
-  {.type = INST_PUSH, .value = 20},
-  {.type = INST_PUSH, .value = 25},
-  {.type = INST_PUSH, .value = 30},
+    DEF_INST_PUSH(10),
+    DEF_INST_PUSH(5),
+    DEF_INST_ADD()
 };
 
-#define PROGRAM_SIZE (sizeof(program)/sizeof(program[0]))
+#define PROGRAM_SIZE (sizeof(program) / sizeof(program[0]))
 #define MAX_STACK_SIZE 1024
+
 int stack[MAX_STACK_SIZE];
-int stack_size = 0;
+int stack_size;
 
 void push(int value) {
-
-  if(stack_size >= MAX_STACK_SIZE) {
-    fprintf(stderr, "ERROR: stack overflow\n");
+  if (stack_size >= MAX_STACK_SIZE) {
+    fprintf(stderr, "ERROR: Stack Overflow\n");
     exit(1);
   }
-
   stack[stack_size] = value;
   stack_size++;
 }
 
 int pop() {
-
-  if(stack_size <= 0) {
-    fprintf(stderr, "ERROR: stack underflow\n");
+  if (stack_size <= 0) {
+    fprintf(stderr, "ERROR: Stack Underflow\n");
     exit(1);
   }
-
-  return stack[stack_size--];
+  return stack[--stack_size];
 }
 
 void print_stack() {
-  for(int i = stack_size - 1; i >= 0; i--) {
-    printf("%d\n",stack[i]);
+  for (int i = stack_size - 1; i >= 0; i--) {
+    printf("%d\n", stack[i]);
   }
 }
 
-int main(int argc, char** argv) {
-
-int a,b;
-  for(size_t i = 0; i < PROGRAM_SIZE; i++) {
-    switch(program[i].type) {
-      case INST_PUSH:
-        push(program[i].value);
-        break;
-      case INST_POP:
-        pop();
-        break;
-      case INST_ADD:
-        a = pop();
-        b = pop();
-        push(a+b);
-        break;
-      case INST_SUB:
-        a = pop();
-        b = pop();
-        push(b-a);
-        break;
-      case INST_MUL:
-        a = pop();
-        b = pop();
-        push(a*b);
-        break;
-      case INST_DIV:
-        a = pop();
-        b = pop();
-        push(b/a);
-        break;
-      case INST_PRINT:
-        printf("%d\n",pop());      
-        break;
+int main() {
+  int a, b;
+  for (size_t ip = 0; ip < PROGRAM_SIZE; ip++) {
+    switch (program[ip].type) {
+    case INST_PUSH:
+      push(program[ip].value);
+      break;
+    case INST_POP:
+      pop();
+      break;
+    case INST_ADD:
+      a = pop();
+      b = pop();
+      push(a + b);
+      break;
+    case INST_SUB:
+      a = pop();
+      b = pop();
+      push(b - a);
+      break;
+    case INST_MUL:
+      a = pop();
+      b = pop();
+      push(a * b);
+      break;
+    case INST_DIV:
+      a = pop();
+      b = pop();
+      push(b / a);
+      break;
+    case INST_PRINT:
+      printf("%d\n", pop());
+      break;
     }
   }
-  print_stack();
 
-  // if(argc < 2) {
-  //   printf("Usage: ./build/ptvm <filename> \n");
-  //   exit(1);
-  // }
-  printf("Build: %s\n",argv[0]); 
-  printf("File: %s\n", argv[1]);
+  print_stack();
   return 0;
 }
